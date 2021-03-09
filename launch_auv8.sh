641d9eb7efa6822026042c4076a64b7c230f634b
sonia-auv <club.sonia@etsmtl.net>
2021-03-08 19:33:32 -0500

    cleanup in launch.sh

--- launch_auv8.sh
+++ launch_auv8.sh
@@ -1,7 +1,6 @@
-XAVIER=$(hostname -I | awk '{print $1}')
-echo $XAVIER
+echo $(hostname -I | awk '{print $1}')
 
-export XAVIER_IP=$XAVIER
+export XAVIER_IP=$(hostname -I | awk '{print $1}')
 
 docker rm $(docker ps -a -q)
 docker-compose -f docker-compose.auv8.yml up
