ebde5246b922fa1f81909af0ed42321e7e9617cf
Kam0ss <camille.sauvain.1@etsmtl.net>
2022-07-09 12:42:03 -0400

    added chmod for config_filterchain folder

--- launch_auv7.sh
+++ launch_auv7.sh
@@ -1,4 +1,5 @@
 export ADRESS_IP=$(hostname -I | awk '{print $1}')
 export AUV=AUV7
+chmod -R a+rwx "/home/sonia/ssd/config_filterchain"
 docker rm $(docker ps -a -q)
 docker-compose -f docker-compose.auv7.yml up
--- launch_auv8.sh
+++ launch_auv8.sh
@@ -1,4 +1,5 @@
 export ADRESS_IP=$(hostname -I | awk '{print $1}')
 export AUV=AUV8
+chmod -R a+rwx "/home/sonia/ssd/config_filterchain"
 docker rm $(docker ps -a -q)
 docker-compose -f docker-compose.auv8.yml up
