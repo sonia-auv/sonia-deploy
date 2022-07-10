88999f39a28ff4d058b9c3502beaf9a966ab3b75
sonia-dummy <tresorier.club.sonia@etsmtl.net>
2022-07-10 03:34:32 -0400

    added chmod to be sure

--- launch_local_auv7.sh
+++ launch_local_auv7.sh
@@ -1,6 +1,6 @@
 export ADRESS_IP=$(hostname -I | awk '{print $1}')
 export AUV=LOCAL
 export LOCAL_AUV=AUV7
-chmod -R a+rwx "$ROS_SONIA_WS/src/config_filterchain"
+sudo chmod -R a+rwx "$ROS_SONIA_WS/src/config_filterchain"
 docker rm $(docker ps -a -q)
 docker-compose -f docker-compose.local.yml up
