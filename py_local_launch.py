bc543acf0e5b60cf97366e3228811bf8ef47d790
Nimai Jariwala <nimai.jariwala.1@ens.etsmtl.ca>
2023-04-09 15:40:24 -0400

    Pylint

--- py_local_launch.py
+++ py_local_launch.py
@@ -1,52 +1,75 @@
-
+"""
+Launch Local Nodes for simulation.
+"""
 import subprocess
 from time import sleep
 import os
 import argparse
-from typing import List, Optional, Union
+from typing import List, Optional
+# pylint: disable=too-few-public-methods, consider-using-with
 
-class Core:
-    
+class BadLaunchException(Exception):
+    """
+    Bad Launch Exception.
+    """
     def __init__(self) -> None:
-        self.process = subprocess.Popen("roscore")
-        LaunchControl.add_node(self)
-    
-    def __del__(self):
-        self.process.terminate()
-        self.process.wait()
+        super().__init__("Package or Node not defined")
 
 class Launch:
-    
-    def __init__(self, pkg, node) -> None:
-        self.process = subprocess.Popen(["roslaunch", pkg, f"{node}.launch"])
+    """
+    Class Object that Launches Roslaunch Nodes.
+    """
+    def __init__(self, pkg=None, node=None, is_core=False) -> None:
+        if is_core:
+            self.process = subprocess.Popen("roscore")
+        elif pkg is not None and node is not None:
+            self.process = subprocess.Popen(["roslaunch", pkg, f"{node}.launch"])
+        else:
+            raise BadLaunchException()
+
         LaunchControl.add_node(self)
-    
+
     def __del__(self):
         self.process.terminate()
         self.process.wait()
 
 class LaunchControl:
+    """
+    Class Object that contains Launched Nodes.
+    """
     __instance: Optional['LaunchControl'] = None
-    __launchedNodes: List[Union[Launch, Core]] = []
+    __launchedNodes: List[Launch] = []
 
     def __new__(cls, *args, **kwargs) -> 'LaunchControl':
         if cls.__instance is None:
             cls.__instance = super().__new__(cls, *args, **kwargs)
         return cls.__instance
-    
+
     @classmethod
-    def add_node(cls, node):
+    def add_node(cls, node: Launch):
+        """
+        Add node to list to keep alive.
+
+        Args:
+            node (Union[Launch, Core]): Node to store.
+        """
         cls.__launchedNodes.append(node)
 
-def main(auv):
-    
+def main(auv: str):
+    """
+    Main script to launch local nodes.
+
+    Args:
+        auv (str): AUV to launch.
+    """
+
     # local env setuo
     os.environ["AUV"] = "LOCAL"
     os.environ["LOCAL_AUV"] = auv
-    
+
     # Start Core
-    Core()
-    
+    Launch(is_core=True)
+
     sleep(1)
 
     # Launch Nodes
@@ -65,6 +88,9 @@ def main(auv):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--auv", type=str, default="AUV8", help="Which Local AUV to use. Defaults to AUV8")
-    args = parser.parse_args()
-    main(args.auv)
+    parser.add_argument("-a", "--auv",
+                        type=str,
+                        default="AUV8",
+                        help="Which Local AUV to use. Defaults to AUV8")
+    parsed_args = parser.parse_args()
+    main(parsed_args.auv)
